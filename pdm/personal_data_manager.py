2b199e7a5dc8b57df563ae52bdb47e8b4d7d56e5
wojtek <wojtek.biniek@gmail.com>
2018-12-28 00:47:43 +0100

    PersonalDataManager class introduction with unittests
    This change introduces PersonalDataManager class with
    unittests. In this state class is able to handle
    input data processing in format of sequence of
    lists and/or tuples and/or dicts and translate it
    internally as set of _PersonalData objects

---
+++ pdm/personal_data_manager.py
@@ -0,0 +1,27 @@
+"""Module contains basic classes for personal data management"""
+import collections
+
+_PersonalData = collections.namedtuple('_PersonalData', 'name address phone_number')
+
+
+class PersonalDataManager:
+    """
+    PersonalDataManager class is a wrapper for _PersonalData class instances
+    """
+
+    def __init__(self, input_data, skip_on_errors=False):
+        """
+        Initializes PersonalDataManager object
+        :param input_data: iterable, should contains
+        """
+        self.data_records = set()
+
+        for record in input_data:
+            if isinstance(record, tuple) or isinstance(record, list):
+                self.data_records.add(_PersonalData(*record))
+            elif isinstance(record, dict):
+                self.data_records.add(_PersonalData(**record))
+            elif not skip_on_errors:
+                exc_message = '{type_name} is not supported data type. Only tuples and/or dicts'.format(
+                    type_name=type(record))
+                raise TypeError(exc_message)
---
+++ pdm/tests/test_personal_data_manager.py
@@ -0,0 +1,73 @@
+"""Unittests for personal_data_manager module"""
+from unittest import TestCase
+
+from pdm.personal_data_manager import PersonalDataManager, _PersonalData
+
+
+class TestPersonalDataManager(TestCase):
+
+    def test_init_objectCreationWithEmptyData_objectCreatedWithNoData(self):
+        # Arrange
+        input_data = []
+
+        # Act
+        pdm = PersonalDataManager(input_data)
+
+        # Assert
+        self.assertEqual(0, len(pdm.data_records))
+        self.assertIsInstance(pdm.data_records, set)
+
+    def test_init_objectCreationWithValidData_objectCreatedWithProvidedData(self):
+        # Arrange
+        data_entry = ('name', 'address', 'phone_number')
+        data_entry2 = ('name2', 'address2', 'phone_number2')
+        input_data = [data_entry, data_entry2]
+
+        # Act
+        pdm = PersonalDataManager(input_data)
+
+        # Assert
+        self.assertEqual(2, len(pdm.data_records))
+
+    def test_init_objectCreationWithDuplicatedData_validNUmberOfRecordsExists(self):
+        # Arrange
+        data_entry = ('name', 'address', 'phone_number')
+        input_data = [data_entry, data_entry]
+
+        # Act
+        pdm = PersonalDataManager(input_data)
+
+        # Assert
+        self.assertEqual(1, len(pdm.data_records))
+
+    def test_init_objectCreationFromTuplesAndDicts_dataRecordsRepresentedAsPersonalDataObjects(self):
+        # Arrange
+        data_entry = ('name', 'address', 'phone_number')
+        data_entry2 = {'name': 'name2', 'address': 'address2', 'phone_number': 'phone_number2'}
+        input_data = [data_entry, data_entry2]
+
+        # Act
+        pdm = PersonalDataManager(input_data)
+
+        # Assert
+        for obj in pdm.data_records:
+            self.assertIsInstance(obj, _PersonalData)
+
+    def test_init_objectCreationInvalidInputData_typeErrorRaised(self):
+        # Arrange
+        input_data = ['invalid_input_data']
+
+        # Act / Assert
+        self.assertRaises(TypeError, PersonalDataManager, input_data)
+
+    def test_init_objectCreationInvalidInputData_validNumberOfRecordsExists(self):
+        # Arrange
+        input_data = ['invalid_input_data', ('name', 'address', 'phone_number')]
+
+        # Act
+        pdm = PersonalDataManager(input_data, skip_on_errors=True)
+
+        # Assert
+        self.assertEqual(1, len(pdm.data_records))
+
+
