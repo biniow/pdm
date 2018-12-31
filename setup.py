8f4cf8f690b35d1848e43540598a17e8034c8a18
wojtek <wojtek.biniek@gmail.com>
2018-12-31 12:29:14 +0100

    setup.py changes - module added and version updated
    This change modifies setup.py containing package
    configuration. Module pdm.data_processors has been
    added to avoid import errors. Moreover version has
    been updated due to last release v1.0.0

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='pdm',
-      version='1.0.0',
+      version='1.1.0',
       description='Python framework for personal data (name, address, phone number) management',
       url='https://github.com/biniow/pdm',
       author='Wojciech Biniek',
       author_email='wojtek.biniek@gmail.com',
       license='MIT',
-      packages=['pdm'],
+      packages=['pdm', 'pdm.data_processors'],
       scripts=['bin/pdm-cli'],
       zip_safe=False)
