b9171c6ab97ad50a46938a25a4cb3f04014c0787
wojtek <wojtek.biniek@gmail.com>
2018-12-31 15:09:43 +0100

    Increment of development version to 1.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pdm',
-      version='1.1.0',
+      version='1.2.0',
       description='Python framework for personal data (name, address, phone number) management',
       url='https://github.com/biniow/pdm',
       author='Wojciech Biniek',
